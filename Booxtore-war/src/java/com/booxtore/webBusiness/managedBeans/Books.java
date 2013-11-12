581e531470b4b610a05dbf7b07092564275e3511
netbean <netbean@netbean>
2013-11-12 10:45:49 +0100

    Merge origin/master

--- Booxtore-war/src/java/com/booxtore/webBusiness/managedBeans/Books.java
+++ Booxtore-war/src/java/com/booxtore/webBusiness/managedBeans/Books.java
@@ -9,11 +9,16 @@ package com.booxtore.webBusiness.managedBeans;
 import com.booxtore.business.BookAccessorLocal;
 import com.booxtore.entity.Book;
 import com.booxtore.entity.Category;
+import com.sun.istack.logging.Logger;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -28,6 +33,12 @@ public class Books {
     // Utile pour la recherche de livres
     private String keywords = "Recherchez ici !";
     private List<Book> books;
+    
+    private int categoryId;
+    private String categoryName;
+    private String categorySummary;
+    private String categoryKeywords;
+
     /**
      * Creates a new instance of Books
      */
@@ -55,9 +66,55 @@ public class Books {
     public Category getCategoryById(int id){
         return bookAccessor.getCategoryById(id);
     }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategorySummary() {
+        return categorySummary;
+    }
+
+    public void setCategorySummary(String categorySummary) {
+        this.categorySummary = categorySummary;
+    }
+
+    public String getCategoryKeywords() {
+        return categoryKeywords;
+    }
+
+    public void setCategoryKeywords(String categoryKeywords) {
+        this.categoryKeywords = categoryKeywords;
+    }
     
-    public void updateCategory(int id, String name, String summary, String keywords){
-        bookAccessor.updateCategory(id, name, summary, keywords);
+    
+    
+    public void updateCategory(){
+        System.out.println(categoryId);
+        System.out.println(categoryName);
+        System.out.println(categorySummary);
+        System.out.println(categoryKeywords);
+
+        bookAccessor.updateCategory(categoryId, categoryName, categorySummary, categoryKeywords);
+         ExternalContext context =  FacesContext.getCurrentInstance().getExternalContext();
+        try{
+            context.redirect(context.getRequestContextPath()+"/admin_area/category.html?id=0");
+        } catch (IOException e){
+            System.err.println("Echec de la mise à jour de la catégorie");
+        }
+        
     }
     
     /**
@@ -125,6 +182,21 @@ public class Books {
         }
         else {
            return listBooks(0); 
+        }
+    }
+    
+    public void loadCategory(int idCategory){
+        Category c = null;
+        if( idCategory > 0 ) c = bookAccessor.getCategoryById(idCategory);
+        else {
+            categoryId = 0;
+        }
+        
+        if(c != null){
+            categoryId = idCategory;
+            categoryName = c.getCategoryName();
+            categorySummary = c.getCategorySummary();
+            categoryKeywords = c.getCategoryKeywords();
         }
     }
 }
